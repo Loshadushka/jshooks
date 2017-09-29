1d2d5b1e269b125ef790c3cfda94b33c1666e2a5
Loshadushka <Loshadushka@users.noreply.github.com>
2017-09-29 15:46:40 +0300

    tbsSpreadsheet changes to be in compliance with implementation

--- workingWithQestionsV85.js
+++ workingWithQestionsV85.js
@@ -115,7 +115,7 @@ function testing_workingWithQuestion(question) {
 
                 document.querySelectorAll("div.assessment_question:not([style='display: none;']) input:not([style='border: none; background: lightgrey; color: black;']").forEach(function (item, index) {
 
-                    item.value = question.correctAnswers[index];
+                    item.value = values(question.correctAnswers[index])[0];
                     $(".assessment_question[data-guid='" + question.guid + "'] input").eq(index).change();
                 })
 

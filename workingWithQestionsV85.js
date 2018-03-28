9a6e264d1c0884f7a6375423ed2fe2e03fb0cd64
Loshadushka <Loshadushka@users.noreply.github.com>
2018-03-28 20:52:38 +0300

    tbsSpreadsheetMixed are fixed for AICPA

--- workingWithQestionsV85.js
+++ workingWithQestionsV85.js
@@ -4,15 +4,15 @@ function testing_workingWithQuestion(question) {
             testing_wholeProcessedAnswersCount = 0;
             testing_delay = 500;
             testing_questionPointer = 0;
-            testing_pressButtonInARow = setTimeout(function () {
+            testing_pressButtonInARow = setTimeout(function() {
                 var answer = question.correctAnswers[testing_questionPointer] - testing_wholeProcessedAnswersCount - 1;
-                setTimeout(function (i) {
+                setTimeout(function(i) {
                     document.querySelectorAll("div.assessment_question:not([style='display: none;']) span.answer_choice")[i].click();
                 }, 100, testing_questionPointer);
-                setTimeout(function (i, answer) {
+                setTimeout(function(i, answer) {
                     document.querySelectorAll("div.assessment_question:not([style='display: none;']) div.answer_box")[i].querySelectorAll("label")[answer].click();
                 }, 200, testing_questionPointer, answer);
-                setTimeout(function (i) {
+                setTimeout(function(i) {
                     document.querySelectorAll("div.assessment_question:not([style='display: none;']) div.button_box")[i].querySelector("button.accept_button").click();
                 }, 300, testing_questionPointer);
                 testing_wholeProcessedAnswersCount = testing_wholeProcessedAnswersCount + document.querySelectorAll("div.assessment_question:not([style='display: none;']) div.answer_box")[testing_questionPointer].querySelectorAll("label").length;
@@ -24,9 +24,7 @@ function testing_workingWithQuestion(question) {
                 }
             }, testing_delay);
 
-
         }
-
 
     }
     if (question.kind == "tbsResearch") {
@@ -34,16 +32,13 @@ function testing_workingWithQuestion(question) {
 
             if (document.querySelectorAll("div.assessment_question[data-guid='" + question.guid + "']:not([style='display: none;']) input:not([disabled])").length == 4) {
 
-
-                document.querySelectorAll("div.assessment_question[data-guid='" + question.guid + "']:not([style='display: none;']) input:not([disabled])").forEach(function (item, index) {
+                document.querySelectorAll("div.assessment_question[data-guid='" + question.guid + "']:not([style='display: none;']) input:not([disabled])").forEach(function(item, index) {
 
                     item.value = question.correctAnswers[0][index];
                     $(".assessment_question[data-guid='" + question.guid + "'] input:not([disabled])").eq(index).keyup();
                 })
 
-
-            }
-            else {
+            } else {
                 document.querySelector(".assessment_question[data-guid='" + question.guid + "'] select:not([disabled])").value = question.correctAnswers[0][0];
                 document.querySelectorAll("div.assessment_question:not([style='display: none;']) input:not([disabled])")[0].value = question.correctAnswers[0][1];
                 document.querySelectorAll("div.assessment_question:not([style='display: none;']) input:not([disabled])")[1].value = question.correctAnswers[0][2];
@@ -51,9 +46,7 @@ function testing_workingWithQuestion(question) {
             }
         }
 
-
     }
-
 
     if (question.kind == "tbsGeneral" && question["sub-kind"] == "infosheet") {
 
@@ -62,15 +55,14 @@ function testing_workingWithQuestion(question) {
             if (document.querySelectorAll("div.assessment_question:not([style='display: none;']) select.mcq").length > 0) {
 
                 testing_wholeProcessedAnswersCount = 0;
-                document.querySelectorAll("div.assessment_question:not([style='display: none;']) .answer_box").forEach(function (item, index) {
+                document.querySelectorAll("div.assessment_question:not([style='display: none;']) .answer_box").forEach(function(item, index) {
                     $(item).dblclick();
                     var answer = question.correctAnswers[index] - testing_wholeProcessedAnswersCount - 1;
                     testing_wholeProcessedAnswersCount = testing_wholeProcessedAnswersCount + document.querySelectorAll(".infosheet_selector li").length;
                     document.querySelectorAll(".infosheet_selector li a")[answer].click();
                     document.querySelector("button.ok-button").click();
                 })
-            }
-            else {
+            } else {
                 for (var i = 0; i < question.correctAnswers.length; i++) {
                     document.querySelectorAll(".assessment_question[data-guid='" + question.guid + "'] .textfield input")[i].value = Object.values(question.correctAnswers[i])[0];
                     $(".assessment_question[data-guid='" + question.guid + "'] .textfield input").eq(i).change();
@@ -81,56 +73,66 @@ function testing_workingWithQuestion(question) {
 
     if (question.kind == "tbsGeneral" && question["sub-kind"] == "yesNo") {
 
-
         if (document.querySelector("div.assessment_question:not([style='display: none;'])").dataset.guid == question.guid) {
 
+            question.correctAnswers.forEach(function(item) {
 
-            question.correctAnswers.forEach(function (item) {
-
-                document.querySelector("div.assessment_question:not([style='display: none;']) #"+item+" +div").click();
+                document.querySelector("div.assessment_question:not([style='display: none;']) #" + item + " +div").click();
 
             })
 
-
         }
 
-
     }
-
 
     if (question.kind == "tbsGeneral" && question["sub-kind"].includes("spreadsheet")) {
 
         if (document.querySelector("div.assessment_question:not([style='display: none;'])").dataset.guid == question.guid) {
 
+            if (question.kind == "tbsGeneral" && question["sub-kind"].includes("Mixed")) {
 
-            if (document.querySelectorAll("div.assessment_question:not([style='display: none;']) input:not([style='border: none; background: lightgrey; color: black;']").length > 0) {
+                question.correctAnswers.forEach(item=>{
+                    if (typeof item == "string") {
 
+                        $("#assessmentWidget div.assessment_question:not([style='display: none;']) select.mcq option[id='" + item + "']").parent().parent().click();
+                        $(".popup_entry_list input[value='" + item + "']").click();
+                        $(".action_accept").click();
+                    } else {
+                        $("#assessmentWidget div.assessment_question:not([style='display: none;']) .text_entry input[id='" + Object.keys(item)[0] + "']").parent().parent().click()
+                        $(".text_entry_input")[0].value = Object.values(item)[0];
+                        $(".text_entry_input").keyup();
+                        $(".action_accept").click();
 
-                document.querySelectorAll("div.assessment_question:not([style='display: none;']) input:not([style='border: none; background: lightgrey; color: black;']").forEach(function (item, index) {
+                    }
 
-                    item.value = Object.values(question.correctAnswers[index])[0];
-                    $(".assessment_question[data-guid='" + question.guid + "'] input").eq(index).change();
-                })
+                }
+                )
 
+            } else {
 
+                if (document.querySelectorAll("div.assessment_question:not([style='display: none;']) input:not([style='border: none; background: lightgrey; color: black;']").length > 0) {
+
+                    document.querySelectorAll("div.assessment_question:not([style='display: none;']) input:not([style='border: none; background: lightgrey; color: black;']").forEach(function(item, index) {
+
+                        item.value = Object.values(question.correctAnswers[index])[0];
+                        $(".assessment_question[data-guid='" + question.guid + "'] input").eq(index).change();
+                    })
+
+                } else {
+
+                    testing_wholeProcessedAnswersCount = 0;
+                    document.querySelectorAll("div.assessment_question:not([style='display: none;']) .answer_box").forEach(function(item, index) {
+                        $(item).dblclick();
+                        var answer = question.correctAnswers[index] - testing_wholeProcessedAnswersCount;
+                        testing_wholeProcessedAnswersCount = testing_wholeProcessedAnswersCount + document.querySelectorAll(".infosheet_selector li").length;
+                        document.querySelectorAll(".infosheet_selector li a")[answer - 1].click();
+                        document.querySelector("button.ok-button").click();
+
+                    })
+                }
             }
-
-            else {
-
-                testing_wholeProcessedAnswersCount = 0;
-                document.querySelectorAll("div.assessment_question:not([style='display: none;']) .answer_box").forEach(function (item, index) {
-                    $(item).dblclick();
-                    var answer = question.correctAnswers[index] - testing_wholeProcessedAnswersCount;
-                    testing_wholeProcessedAnswersCount = testing_wholeProcessedAnswersCount + document.querySelectorAll(".infosheet_selector li").length;
-                    document.querySelectorAll(".infosheet_selector li a")[answer - 1].click();
-                    document.querySelector("button.ok-button").click();
-
-
-                })
-            }
-
-
         }
+
     }
 
 }
